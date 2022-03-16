4a7538a5eacdc55599137ce477780cc33ce560c3
Alex <101295499+lexicypher@users.noreply.github.com>
2022-03-16 10:45:09 +0000

    Add files via upload

---
+++ sport.py
@@ -0,0 +1,10 @@
+import webbrowser, sys
+url = 'https://www.bbc.co.uk/sport/football'
+url1 = 'https://fantasy.premierleague.com/'
+url2 = 'https://theathletic.com/uk/'
+
+webbrowser.open(url)
+webbrowser.open(url1)
+webbrowser.open(url2)
+
+
